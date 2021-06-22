593a704ff2a42bbe617cc43d92718c9af078a9fe
lisbeth-garcia <86217075+lisbeth-garcia@users.noreply.github.com>
2021-06-22 11:07:24 -0500

    Update ActivadadB2 (2).cpp
    Garcia lopez lisbeth hice la parte de Mostrar por pantalla los elementos de la matriz. utilizando las variables do-while que es una estructura repetitiva en la que se caracteriza por ejecutar un conjunto de instrucciones, es decir, las acciones que se realiza por lo menos una vwz pese a obtener un valor falso, mientras que el if se lo usa como condición.

--- ActivadadB2 (2).cpp
+++ ActivadadB2 (2).cpp
@@ -1,3 +1,4 @@
+//Garcia lopez lisbeth Mostrar por pantalla los elementos de la matriz.
 #include<iostream>
 using namespace std;
 int main()
@@ -6,10 +7,25 @@ int c=0;
 float matriz1[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
-
-
-
-
+  do {
+	cout<<matriz1[c];
+	c=c+1;
+	if(c<10){
+		cout<<" - ";
+	}
+}while(c<10);
+cout<<endl;
+c=0;
+do {
+	cout<<matriz2[c];
+	c=c+1;
+	if(c<10){
+		cout<<" - ";
+	}
+}while(c<10);
+cout<<endl;
+c=0;
+  
 return 0;
 
 }
